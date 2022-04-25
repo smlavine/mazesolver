112b8418f157cb1b9ccc628b8d97b163caf5daec
Sebastian LaVine <mail@smlavine.com>
2022-04-25 19:03:36 -0400

    Remove old function

--- mazesolver.go
+++ mazesolver.go
@@ -70,30 +70,7 @@ func (maze *Maze) String() string {
 
 }
 
-func StdinZeroesAndOnes() (zeroes uint, ones uint) {
-	scanner := bufio.NewScanner(os.Stdin)
-
-	scanner.Split(bufio.ScanWords)
-
-	for scanner.Scan() {
-		switch scanner.Text() {
-		case "0":
-			zeroes++
-		case "1":
-			ones++
-		}
-	}
-
-	return zeroes, ones
-}
-
 func main() {
 	maze := NewMaze(os.Stdin)
 	fmt.Print(maze.String())
-	//zeroes, ones := StdinZeroesAndOnes()
-
-	//sumdec := float64(zeroes + ones)
-
-	//fmt.Printf("0s: %d\n1s: %d\n0s ratio: %.2f\n1s ratio: %.2f\n",
-	//	zeroes, ones, float64(zeroes)/sumdec, float64(ones)/sumdec)
 }
